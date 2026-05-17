f96b3299b9917cb79d62b67f5365eed44f9f258e
dhanush3727 <dhanushs4827@gmail.com>
2026-05-17 16:53:18 +0530

    Breakdown module added

---
+++ server/src/modules/breakdown/breakdown.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { BreakdownService } from './breakdown.service';
+import { BreakdownController } from './breakdown.controller';
+
+@Module({
+  providers: [BreakdownService],
+  controllers: [BreakdownController]
+})
+export class BreakdownModule {}
