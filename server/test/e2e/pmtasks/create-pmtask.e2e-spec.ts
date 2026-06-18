2e1d78965ef64992adf9c5b3d839ddf626bd9a1f
dhanush3727 <dhanushs4827@gmail.com>
2026-06-18 15:46:17 +0530

    add test case

--- server/test/e2e/pmtasks/create-pmtask.e2e-spec.ts
+++ server/test/e2e/pmtasks/create-pmtask.e2e-spec.ts
@@ -1,6 +1,8 @@
-import { describe, beforeAll, afterAll } from '@jest/globals';
+import { describe, beforeAll, afterAll, it } from '@jest/globals';
 import { INestApplication } from '@nestjs/common';
 import { createApp } from '../../utils/create-app';
+import request from 'supertest';
+import { LoginResponse } from '../../test.types';
 
 describe('Create PM Task E2E', () => {
   let app: INestApplication;
@@ -11,5 +13,22 @@ describe('Create PM Task E2E', () => {
 
   afterAll(async () => {
     await app.close();
+  });
+
+  it('should create pmtask', async () => {
+    const server = app.getHttpServer() as Parameters<typeof request>[0];
+
+    const payload = {
+      email: `dhanush7825@gmail.com`,
+      password: 'Dhanush@3727',
+    };
+
+    const loginRes = await request(server)
+      .post('/api/auth/login')
+      .send(payload)
+      .expect(200);
+
+    const loginData = loginRes.body as LoginResponse;
+    const accessToken = loginData.data.accessToken;
   });
 });
