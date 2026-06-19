cce24791372c93933d115d1e155c35f1430d6b5d
dhanush3727 <dhanushs4827@gmail.com>
2026-06-19 13:42:42 +0530

    update pm task

---
+++ server/test/e2e/pmtasks/update-pmtask.e2e.spec..ts
@@ -0,0 +1,34 @@
+import { describe, beforeAll, afterAll, it } from '@jest/globals';
+import { INestApplication } from '@nestjs/common';
+import { createApp } from '../../utils/create-app';
+import request from 'supertest';
+import { LoginResponse } from '../../test.types';
+
+describe('Update PM Task E2E', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    app = await createApp();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('should create pmtask', async () => {
+    const server = app.getHttpServer() as Parameters<typeof request>[0];
+
+    const payload = {
+      email: `dhanush7825@gmail.com`,
+      password: 'Dhanush@3727',
+    };
+
+    const loginRes = await request(server)
+      .post('/api/auth/login')
+      .send(payload)
+      .expect(200);
+
+    const loginData = loginRes.body as LoginResponse;
+    const accessToken = loginData.data.accessToken;
+  });
+});
