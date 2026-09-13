bf296b135822f611113351e9e7a735bcb167c3ef
dhanush3727 <dhanushs4827@gmail.com>
2026-09-13 12:32:34 +0530

    test: Change the config

--- client/playwright.config.ts
+++ client/playwright.config.ts
@@ -98,9 +98,18 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: "npm run dev",
-    url: "http://localhost:3000",
-    reuseExistingServer: !process.env.CI,
-  },
+  webServer: [
+    {
+      command: "npm run dev",
+      url: "http://localhost:3000",
+      reuseExistingServer: !process.env.CI,
+    },
+
+    {
+      command: "npm run start:dev",
+      url: "http://localhost:5000/api",
+      reuseExistingServer: !process.env.CI,
+      cwd: "../server",
+    },
+  ],
 });
