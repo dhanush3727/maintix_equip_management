5a0c3d54c474b3d5fd20b7e2ab27a13251b01cc1
dhanush3727 <dhanushs4827@gmail.com>
2026-09-14 07:02:29 +0530

    ci: Remove the PORT

--- client/playwright.config.ts
+++ client/playwright.config.ts
@@ -99,11 +99,11 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: [
-    // {
-    //   command: "npm run dev",
-    //   url: "http://localhost:3000",
-    //   reuseExistingServer: !process.env.CI,
-    // },
+    {
+      command: "npm run dev",
+      url: "http://localhost:3000",
+      reuseExistingServer: !process.env.CI,
+    },
 
     {
       command: "npm run start:dev",
